307d194348f90148fc468d0e4b863e4fc47215f4
QuocEran <quocchuong45@gmail.com>
2022-05-09 13:04:07 +0700

    deploy

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,8 @@ module.exports = {
   devServer: {
     port: 8000,
   },
+  publicPath:
+    process.env.NODE_ENV === "production"
+      ? "/IHS_User/" // Thay tên repository của các bạn vào đây nhé
+      : "/",
 };
